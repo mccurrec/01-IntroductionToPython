7fe315b304344854ae2d3243cd4bd77fc8ba5ddf
mccurrec <mccurrec@rose-hulman.edu>
2018-11-26 17:05:38 -0500

    done

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Ezrie McCurry.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -73,6 +73,7 @@ import rosegraphics as rg
 #   change this _TODO_ to DONE and  ** continue to the next _TODO_ (below). **
 #
 ###############################################################################
+
 
 # -----------------------------------------------------------------------------
 # The next few lines show how to:
@@ -116,7 +117,7 @@ natasha.go_to(rg.Point(-100, 200))
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
@@ -126,7 +127,9 @@ natasha.go_to(rg.Point(-100, 200))
 #   As always, test by running the module.
 #
 ###############################################################################
-
+natasha.draw_regular_polygon(8,45)
+boris.speed = 9
+boris.draw_regular_polygon(12,75)
 ###############################################################################
 #
 # TODO: 5.
@@ -136,7 +139,7 @@ natasha.go_to(rg.Point(-100, 200))
 #
 #   Add code of your own that constructs another SimpleTurtle object,
 #   naming it whatever you want.  Names cannot have spaces or special
-#   characters, but they can have digits and underscores, e.g.
+# #   characters, but they can have digits and underscores, e.g.
 #      this_1_has
 #
 #   STYLE RULE: Your names should always begin with a LOWER_CASE letter.
@@ -153,10 +156,15 @@ natasha.go_to(rg.Point(-100, 200))
 #   As always, test by running the module.
 #
 ###############################################################################
+frank = rg.SimpleTurtle('turtle')
+frank.pen = rg.Pen('purple', 55)
+frank.speed = 8
+frank.forward(150)
+
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
